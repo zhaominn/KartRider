562c86ae0c212f269f72e1adc5238d23d6d3c3b8
neneee0181 <2022180024@tukorea.ac.kr>
2024-12-06 00:38:45 +0900

    update : 모든 모델 draw함수 변경

--- KarKartrider/KarKartrider/main.cpp
+++ KarKartrider/KarKartrider/main.cpp
@@ -44,8 +44,8 @@ int main(int argc, char** argv) {
 
 
 	// play mode에 필요한 모델 로드
-	loadModelWithProgress <KartModel>("kronos.obj", "obj/car/kronos/", "car", "sphere", glm::scale(glm::mat4(1.0f), glm::vec3(10.0, 10.0, 10.0)), karts);
-	loadModelWithProgress <RoadModel>("road_all_1.obj", "obj/road/", "road_all", "cube", glm::scale(glm::mat4(1.0f), glm::vec3(20.0, 20.0, 20.0)), roads);
+	loadModelWithProgress <KartModel>("kronos.obj", "obj/car/kronos/", "car", "sphere", glm::scale(glm::mat4(1.0f), glm::vec3(1.0, 1.0, 1.0)), karts);
+	loadModelWithProgress <RoadModel>("road_all_1.obj", "obj/road/", "road_all", "cube", glm::scale(glm::mat4(1.0f), glm::vec3(100.0, 100.0, 100.0)), roads);
 
 	initializeModelsWithPhysics(karts); // 모든 모델 Bullet world에 추가
 	initializeModelsWithPhysics(roads);
