8a9caa3738cd05c7da7676d75021bdddf0faf79e
neneee0181 <2022180024@tukorea.ac.kr>
2024-12-06 20:04:19 +0900

    update : selectmode -> playmode 이동 확인

--- KarKartrider/KarKartrider/SelectMapMode.h
+++ KarKartrider/KarKartrider/SelectMapMode.h
@@ -4,8 +4,7 @@
 #include "shaderMaker.h"
 #include "root.h"
 #include "KeyBoard.h"
-#include "LoadImg.h"
-
+#include "PlayMode.h"
 
 class SelectMapMode : public Mode {
 public:
@@ -13,11 +12,18 @@ public:
 	SelectMapMode() {}
 
 	void init() override {
-
 	}
 
 	void keyboard(unsigned char key, int x, int y) override {
-
+		switch (key) {
+		case '\r': {
+			PlayMode* playMode = new PlayMode();
+			MM.SetMode(playMode);
+			break;
+		}
+		default:
+			break;
+		}
 	}
 
 	void draw_model() override {
